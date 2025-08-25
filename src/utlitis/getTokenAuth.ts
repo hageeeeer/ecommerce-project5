ffb080f8f6bdb97ef8ddcafb14c8ae8912c89c2e
HajarsaMer <hagersamer21@gmail.com>
2025-08-26 00:18:48 +0300

    update cookie

--- src/utlitis/getTokenAuth.ts
+++ src/utlitis/getTokenAuth.ts
@@ -3,7 +3,10 @@ import { decode } from "next-auth/jwt"
 import { cookies } from "next/headers"
 export async function getTokenAuth()
 {
-    const authToken = (await cookies()).get('next-auth.session-token')?.value
+    const cookieName = process.env.NODE_ENV === 'production' 
+  ? '__Secure-next-auth.session-token'
+  : 'next-auth.session-token'
+    const authToken = (await cookies()).get(cookieName)?.value
     const token = await decode({token:authToken,secret:process.env.NEXTAUTH_SECRET!})
     return token?.token 
 }
